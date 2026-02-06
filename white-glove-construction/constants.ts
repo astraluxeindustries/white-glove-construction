64504aba10c99bc9725ded374d8cbd9a40a630bd
astraluxeindustries <astraluxeindustries@gmail.com>
2026-02-05 17:50:45 -0800

    Update constants.ts

--- white-glove-construction/constants.ts
+++ white-glove-construction/constants.ts
@@ -193,6 +193,7 @@ export const GALLERY_SECTIONS = [
       "/images/new-concrete-garage-improvement.jpg",
       "/images/improved-landscape-patio.jpg",
       "/images/improved-floor-entryway-casing.jpg",
+   },
 ];
 
 // Kept for backward compatibility if any component strictly requires a flat list,
